460daf64c188ee344558b2d3d8567a7d3ca7c717
anko.i1026@gmail.com <airi@inoueairinoMacBook-Pro.local>
2024-02-21 14:40:53 +0900

    create components

---
+++ vue-project/src/components/TodoApp.vue
@@ -0,0 +1,15 @@
+
+import TodoListView from './TodoListView.vue';
+
+import TodoList from './TodoList.vue';
+<script setup>
+import TodoList from './TodoList.vue';
+import TodoListView from './TodoListView.vue';
+</script>
+
+<template>
+  <div class="todoApp">
+    <TodoList/>
+    <TodoListView/>
+  </div>
+</template>
